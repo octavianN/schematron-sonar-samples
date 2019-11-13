bb64a1e92bfa379b2aa8934dc862273540c38d34
Octavian <octavian_nadolu@sync.ro>
2019-11-13 11:22:21 +0100

    Update sonar.sh

--- validation/sonar.sh
+++ validation/sonar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading sonar"
-
+mkdir -p bin
 if [[ ! -e bin/sonar ]];
 then
   curl -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip > bin/sonar.zip
